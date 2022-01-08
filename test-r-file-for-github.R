121bdb9a5997de3f4f30e5099b7f9059e02bce21
SimiDavid95 <71892714+SimiDavid95@users.noreply.github.com>
2022-01-07 20:48:15 -0500

    Add files via upload
    This file is a test R file to experiment with uploading files to GitHub. In this file, you will find some algebraic equations.

---
+++ test-r-file-for-github.R
@@ -0,0 +1,13 @@
+# This is a test R file to practice uploading files to GitHub
+
+# For your enjoyment, I will do some algebraic exercises below:
+
+x <- 2
+y <- 4
+
+x + y
+2*x + 3*y
+x + 10^x
+y - 6^y
+
+# This marks the end of the file
